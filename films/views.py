f1b54458519ffe72fed912a5b4b5380a685c3624
Beni <benik@live.ca>
2022-10-25 10:31:42 -0400

    using htmx and custom css

--- films/views.py
+++ films/views.py
@@ -26,6 +26,6 @@ class RegisterView(FormView):
 def check_username(request):
     username = request.POST.get('username')
     if get_user_model().objects.filter(username=username).exists():
-        return HttpResponse("This username already exists")
+        return HttpResponse("<div id='username-error' class='error'>This username already exists</div>")
     else:
-        return HttpResponse("")
+        return HttpResponse("<div id='username-error' class='success'>This username is available</div>")
